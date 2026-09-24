1e15bbd6edc9d77d98582cc735d960c9d2baf5cf
Viajero-del-Tiempo <arnaldojavierguerrero@gmail.com>
2026-09-24 10:30:45 -0300

    feat: add remote web version cache configuration for whatsapp client

--- src/config/whatsapp.js
+++ src/config/whatsapp.js
@@ -7,6 +7,11 @@ export const whatsappClient = new Client({
     dataPath: "./sessions",
     clientId: "client-one",
   }),
+  webVersionCache: {
+    type: "remote",
+    remotePath:
+      "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+  },
   puppeteer: {
     headless: true,
     executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
